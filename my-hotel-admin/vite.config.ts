d4b857cfd4918837ed7b5dac1a3a61b69ca8face
TVinhNguyen <tvinhNguyen@users.noreply.github.com>
2025-12-23 21:33:49 +0700

    update admin allowedhost

--- my-hotel-admin/vite.config.ts
+++ my-hotel-admin/vite.config.ts
@@ -3,4 +3,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "admin.thahvinhdevops.online",
+      "*.thahvinhdevops.online",
+    ],
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
